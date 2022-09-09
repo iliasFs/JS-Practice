ce65508782247b17e84d07b6d616f3a40a534c13
ilias <ilias.fasilakhs@gmail.com>
2022-09-09 13:47:38 +0300

    practice Loops

---
+++ App.js
@@ -0,0 +1,115 @@
+// console.log('sdkfjnskdf');
+// console.log("Heloow world" + " lets start") 
+// console.log("Fasilakis" [0])
+
+// console.log('1231413423'.length)
+
+
+
+// let myNumber = "ilias fasilakis";
+// console.log(myNumber.length);
+
+
+// console.log(myNumber[myNumber.length - 1])
+
+// let myName = 'ilias';
+// let mySurName = 'fasilakis';
+
+// console.log('my name is ' + myName + " " + mySurName );
+
+// myName = myName + " Hello World";
+// console.log(myName);
+
+// let celsius = 10;
+// let fahrenheit = celsius*1.8 + 32;
+// console.log(fahrenheit);
+
+
+
+// let number1 = '10';
+
+// let number2 = 10;
+
+// number1 == number2;
+// console.log(number2);
+
+
+// let subscribed = false;
+//  if(subscribed === true)
+//  {
+//     console.log("You have subscribed")
+
+// }
+// console.log('Please subscribe first');
+
+// let cash = 100;
+// let price =120;
+
+// if (cash > price)
+
+// {
+    
+//     console.log(`you paid more.. here is your change ${cash - price} `)
+
+// }
+
+// else if(cash === price)
+// {
+
+//     console.log(`You paid the exct ammount. Have a nice day!`)
+// }
+
+//     else {
+
+//         console.log(`Not enough cash. You need ${price - cash} more`)
+
+//     }
+
+ 
+
+
+/** TERNARY OPERATORS */
+
+//     let hot = true;
+
+// hot? console.log("its hot out there..") : console.log('its cold here');
+
+
+
+// let subscribed = true;
+// let loggedIn = true;
+
+// let str = subscribed && loggedIn ? 'show the video' : "you need to login and subscribe to see the video";
+// console.log(str);
+
+
+// let cash = 50;
+// let price = 100;
+// let isStoreOpen = true;
+
+// let result = price<=cash && isStoreOpen ? "give receipt" : "do not give receipt";
+// console.log(result);
+
+
+
+
+
+
+/*LOOPS ------- DRY (Dont Repeat Yourself)*/
+/*count from 1 to 100*/
+
+let count = 1;
+
+
+while(count<=100) 
+{
+    console.log(count);
+    count+=1;
+    
+
+}
+
+
+
+
+
