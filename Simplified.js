dabbdc710ae0315be09b089e8d6d7720a5d7c1c2
ilias <ilias.fasilakhs@gmail.com>
2022-10-02 12:18:47 +0300

    keep learning js functions as arguments, arrow functions

---
+++ Simplified.js
@@ -0,0 +1,29 @@
+// console.log("Hello world");
+
+/*call back functions*/
+
+// function printVariable(variable)
+// {
+// console.log(variable);
+
+// }
+
+// function printString(name, callback){
+
+//     callback('Hello ' + name)
+// }
+
+// printString('ilias', (a)=>{
+
+//     console.log(a)
+// })
+
+// function printHi(name){
+
+//     console.log('hi')
+
+// }
+
+// let printHi = (name) => console.log(name);
+
+// printHi("ilias");
