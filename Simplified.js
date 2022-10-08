056ee1dde0afaf2eebc76f4ff3bf88e06dedeaca
ilias <ilias.fasilakhs@gmail.com>
2022-10-08 21:22:11 +0300

    js practice

--- Simplified.js
+++ Simplified.js
@@ -150,49 +150,72 @@
 //   console.log(firstPassenger.maria)
 
 
-class HospitalEmployee {
-    constructor(name) {
-      this._name = name;
-      this._remainingVacationDays = 20;
-    }
+// class HospitalEmployee {
+//     constructor(name) {
+//       this._name = name;
+//       this._remainingVacationDays = 20;
+//     }
     
-    get name() {
-      return this._name;
-    }
+//     get name() {
+//       return this._name;
+//     }
     
-    get remainingVacationDays() {
-      return this._remainingVacationDays;
-    }
+//     get remainingVacationDays() {
+//       return this._remainingVacationDays;
+//     }
     
-    takeVacationDays(daysOff) {
-      this._remainingVacationDays -= daysOff;
-    }
-  }
+//     takeVacationDays(daysOff) {
+//       this._remainingVacationDays -= daysOff;
+//     }
+//   }
   
-  class Nurse extends HospitalEmployee {
-    constructor(name, certifications) {
-      super(name);
-      this._certifications = certifications;
-    } 
+//   class Nurse extends HospitalEmployee {
+//     constructor(name, certifications) {
+//       super(name);
+//       this._certifications = certifications;
+//     } 
     
-    get certifications() {
-      return this._certifications;
-    }
+//     get certifications() {
+//       return this._certifications;
+//     }
     
-    addCertification(newCertification) {
-      this.certifications.push(newCertification);
-    }
-  }
+//     addCertification(newCertification) {
+//       this.certifications.push(newCertification);
+//     }
+//   }
   
-  const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
-  nurseOlynyk.takeVacationDays(5);
-  console.log(nurseOlynyk.remainingVacationDays);
-  nurseOlynyk.addCertification('Genetics');
-  console.log(nurseOlynyk.certifications);
+//   const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+//   nurseOlynyk.takeVacationDays(5);
+//   console.log(nurseOlynyk.remainingVacationDays);
+//   nurseOlynyk.addCertification('Genetics');
+//   console.log(nurseOlynyk.certifications);
   
   
   
+// function AlphabetSoup(str) { 
 
+//     // code goes here  
+//    let newString =  str.split('').sort().join('');
+  
+//     return newString
+   
+//   }
+
+//   console.log(AlphabetSoup('hello'))
+
+
+
+
+
+//   const paragrafos = document.getElementById("demo")
+//   AlphabetSoup(array,)
+
+
+
+
+
+
+  
 
 
 
