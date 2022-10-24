c8074d096e6a11f2a95ca6ac994d384a3447d4bf
ilias <ilias.fasilakhs@gmail.com>
2022-10-24 20:27:30 +0300

    JS practice and courses

--- Simplified.js
+++ Simplified.js
@@ -688,3 +688,47 @@
 // }
 
 // console.log(doubleChar("Adidas"));
+
+// Input: ["X:-1", "Y:1", "X:-4", "B:3", "X:5"]
+// Output: B:3,Y:1
+
+
+function CountingMinutesI(str) { 
+
+    var split = str.split('-')
+  
+    function toMinutes(time) {
+  
+      var hour = parseInt(time.split(':')[0])
+      var minutes = parseInt(time.slice(0, time.length - 2).split(':')[1])
+      var totalMin = hour*60 + minutes
+      
+      if (time[time.length - 2] === 'p')
+        if (hour !== 12)
+          return totalMin + 720
+        else
+          return totalMin
+      else
+        if (hour === 12)
+          return totalMin + 720
+        else
+          return totalMin
+    }
+    
+    var time1 = toMinutes(split[0])
+    var time2 = toMinutes(split[1])
+    
+    if ( time1 > time2 )
+      return (1440 - time1) + time2
+    else
+      return time2 - time1
+  }
+  
+     
+  // keep this function call here 
+  CountingMinutesI(readline());
+  
+  
+
+console.log(GroupTotals(["X:-1", "Y:1", "X:-4", "B:3", "X:5"]))
+   
